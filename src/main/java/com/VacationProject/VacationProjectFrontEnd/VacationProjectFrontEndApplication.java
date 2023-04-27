5e11a868471e881c719a26adc2d21d3c1387ab1f
Arnoldas <arnoldaspetrauskas112@gmail.com>
2023-04-27 18:46:56 +0300

    first commit

---
+++ src/main/java/com/VacationProject/VacationProjectFrontEnd/VacationProjectFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.VacationProject.VacationProjectFrontEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationProjectFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationProjectFrontEndApplication.class, args);
+	}
+
+}
